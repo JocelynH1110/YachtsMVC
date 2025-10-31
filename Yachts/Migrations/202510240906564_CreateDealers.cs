840d368b34326b76952131d9a292136c8c2ca2eb
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-10-31 19:38:16 +0800

    feature Dealers model, DbContext, Migrations

---
+++ Yachts/Migrations/202510240906564_CreateDealers.cs
@@ -0,0 +1,36 @@
+﻿namespace Yachts.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateDealers : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Dealers",
+                c => new
+                    {
+                        DealerId = c.Int(nullable: false, identity: true),
+                        CompanyName = c.String(nullable: false, maxLength: 200),
+                        Contact = c.String(nullable: false, maxLength: 100),
+                        Address = c.String(),
+                        Tel = c.String(nullable: false, maxLength: 30),
+                        Fax = c.String(maxLength: 30),
+                        Email = c.String(maxLength: 200),
+                        Website = c.String(maxLength: 200),
+                        CreatedAt = c.DateTime(nullable: false),
+                        UpdatedAt = c.DateTime(nullable: false),
+                        SortOrder = c.Int(nullable: false),
+                        CountryCode = c.String(nullable: false, maxLength: 2),
+                    })
+                .PrimaryKey(t => t.DealerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Dealers");
+        }
+    }
+}
---
+++ Yachts/Models/Dealer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Yachts.Models
+{
+    public class Dealer
+    {
+        public int DealerId { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Contact { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Tel { get; set; }
+
+        [StringLength(30)]
+        public string Fax { get; set; }
+
+        [StringLength(200)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(200)]
+        [Url]
+        public string Website { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public int SortOrder { get; set; } = 0;
+
+        [Required]
+        [StringLength(2)]
+        public string CountryCode { get; set; }
+    }
+}
