e0ce35aa3664b76fbbb74ae52f190586572da2b6
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2026-02-10 16:30:50 +0800

    Feat:implement yacht repository and add truncate to admin yachts index

---
+++ Yachts/Controllers/YachtsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Yachts.Models;
+using Yachts.Repositories;
+
+namespace Yachts.Controllers
+{
+    public class YachtsController : Controller
+    {
+        private readonly YachtRepository _repo;
+
+        public YachtsController()
+        {
+            _repo=new YachtRepository(new DBModelContext());
+        }
+
+        // GET: Yachts
+        public ActionResult Index(string product)
+        {
+            var result = _repo.GetProducts(product);
+
+            ViewBag.Product = product;
+            ViewBag.Products = _repo.ListYachts();
+           
+            return View(result);
+        }
+    }
+}
---
+++ Yachts/Repositories/YachtRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Yachts.Models;
+
+namespace Yachts.Repositories
+{
+    public class YachtRepository
+    {
+        private readonly DBModelContext _db;
+
+        public YachtRepository(DBModelContext dBModelContext)
+        {
+            _db = dBModelContext;
+        }
+
+        // 取得全部
+        public IQueryable<Product> GetProducts(string productName = null)
+        {
+            var query = _db.Products.AsQueryable();
+
+            // productName 有值就做篩選;沒值 = null 就傳回全部資料
+            if (!string.IsNullOrEmpty(productName))
+            {
+                query=query.Where(p=>p.Name == productName);
+            }
+            return query.OrderBy(p=>p.Name);
+        }
+        public IEnumerable<string> ListYachts()
+        {
+            return _db.Products.Where(p=>!string.IsNullOrEmpty(p.Name)).Select(p=>p.Name).Distinct().ToList();
+        }
+    }
+}
