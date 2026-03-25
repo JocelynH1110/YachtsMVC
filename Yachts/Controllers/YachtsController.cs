d86ca2a7132c5aa97b0bd6f27e1a64299f85aeb5
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2026-03-25 23:56:26 +0800

    Feat: create deslplan's front page

--- Yachts/Controllers/YachtsController.cs
+++ Yachts/Controllers/YachtsController.cs
@@ -44,6 +44,7 @@ namespace Yachts.Controllers
 
                 SelectedYacht = new YachtDetailViewModel
                 {
+                    Id= selected.Id,
                     Name = selected.Name,
                     Specification = selected.Specification,
                     Structual = selected.Structual,
@@ -58,10 +59,53 @@ namespace Yachts.Controllers
             return View(vm);
         }
 
-        public ActionResult DeskPlan(int? productId)
+        public ActionResult DeskPlan(int? id)
         {
+            var yachts = _repo.GetProducts();
 
-            return View();
+            if (yachts == null || !yachts.Any())
+            {
+                return HttpNotFound();
+            }
+
+            var selected = id.HasValue ? _repo.GetProductByProductId(id.Value) : yachts.FirstOrDefault();
+
+            if (selected == null)
+            {
+                return HttpNotFound();
+            }
+
+            var yacht=_repo.GetProductByProductId(id.Value);
+
+            if (yacht == null)
+            { 
+            return HttpNotFound();
+            }
+
+            var vm = new YachtPageViewModel
+            {
+                Yachts = yachts.Select(y => new YachtListViewModel
+                {
+                    Id = y.Id,
+                    Name = y.Name
+                }).ToList(),
+
+                SelectedYacht = new YachtDetailViewModel
+                {
+                    Id = selected.Id,
+                    Name = selected.Name,
+                    Specification = selected.Specification,
+                    Structual = selected.Structual,
+
+                    Sizes = selected.Sizes.Select(s => new YachtSizeViewModel
+                    {
+                        DimensionName = s.DimensionName,
+                        DimensionValue = s.DimensionValue,
+                    }).ToList(),
+                }
+            };
+
+            return View(vm);
         }
 
         public ActionResult Specification(int? id) 
--- Yachts/ViewModels/Yacht/YachtDetailViewModel.cs
+++ Yachts/ViewModels/Yacht/YachtDetailViewModel.cs
@@ -8,6 +8,7 @@ namespace Yachts.ViewModels.Yacht
     // 遊艇詳細頁(遊艇頁右方)
     public class YachtDetailViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Structual { get; set; }
         public string Specification { get; set; }
